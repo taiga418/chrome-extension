e00adfb5b2ed0cd1f4d462e5725838cd22a51800
taiga matsumoto <taiga418@gmail.com>
2015-02-19 11:17:24 -0800

    api calls work, displays image and errors accordingly, need to format ui and additional features

--- src/app.js
+++ src/app.js
@@ -4,10 +4,28 @@
 var app = angular.module('app',[]);
 
 
-app.controller('MainController', ['$scope' ,function($scope){
+app.controller('MainController', ['$scope','$http',function($scope,$http){
+  $scope.flag = false;
   $scope.data = 'd';
   $scope.search = function(){
-    console.log('asdf');
-    $scope.data = $scope.querry;
+    $http({
+      url: 'https://ajax.googleapis.com/ajax/services/search/images' +
+    '?v=1.0&q='+ $scope.querry,
+      method: 'GET',
+    }).success(function(data){
+      $scope.flag = false;
+      $scope.errors = "";
+      if(data.responseData.results.length == 0){
+        $scope.errors = "no pic found";
+      }else{
+        $scope.flag = true;
+        $scope.link = data.responseData.results[0].url;
+      }
+      $scope.querry = '';
+    }).error(function(err){
+      $scope.data = err;
+    })
+    
+    //$scope.data = $scope.querry;
   }
 }]);
